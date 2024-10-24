901974c88d12407448fc62ee2c76927c54d24afa
osoto <osoto@sidoo.es>
2024-10-24 16:56:41 +0200

    [ADD] account_asset_initiated_assistant

---
+++ account_asset_initiated_assistant/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Account Asset Initiated Assistant",
+    "version": "16.0.1.0.0",
+    "summary": "Asistant to help initiate assets",
+    "category": "Assets",
+    "author": "Oscar Soto, Sidoo SL",
+    "maintainer": "Sidoo SL",
+    "website": "https://sidoo.es",
+    "license": "AGPL-3",
+    "depends": [
+        "account_asset_management",
+    ],
+    "data": [
+        "data/ir_actions_server_data.xml",
+        "views/account_asset.xml",
+        "views/account_asset_line.xml",
+    ],
+}
