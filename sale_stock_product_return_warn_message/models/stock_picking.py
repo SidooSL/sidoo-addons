9e8367897cb3a56499103c7d4ba6221d6f23d717
IvanDLPG-Sidoo <idelapoza@sidoo.es>
2024-11-27 09:54:11 +0100

    Added module to warn in picking if there is a return product

---
+++ sale_stock_product_return_warn_message/models/stock_picking.py
@@ -0,0 +1,30 @@
+from odoo import fields, models, api, _
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    is_returnable = fields.Boolean(
+        compute='_compute_is_returnable',
+        store=True,
+    )
+
+    warning_message = fields.Char(
+        readonly=True,
+        compute='_compute_warning_message',
+        store=True,
+    )
+
+    @api.depends('is_returnable')
+    def _compute_warning_message(self):
+        warning_param = self.env['ir.config_parameter'].sudo().get_param('returnable.message')
+        for picking in self:
+            if picking.is_returnable:
+                picking.warning_message = warning_param
+            else:
+                picking.warning_message = False
+
+    @api.depends('move_line_ids.is_returnable')
+    def _compute_is_returnable(self):
+        for picking in self:
+            picking.is_returnable = any(line.is_returnable for line in picking.move_line_ids)
