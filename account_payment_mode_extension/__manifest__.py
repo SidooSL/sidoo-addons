5f66c6fc67e280343d62adbb387072a3b14bdf9a
Luis Jimenez Hernández <ljimenez@sidoo.es>
2024-12-18 15:16:10 +0100

    ADD account payment mode extension module

---
+++ account_payment_mode_extension/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Account Payment Mode Extension",
+    "version": "17.0.1.0.0",
+    "category": "Accounting",
+    "author": "Luis Jimenez, Sidoo S.L.",
+    "license": "AGPL-3",
+    "depends": [
+        "account_payment_mode",
+        "account_payment_order",
+    ],
+    "data": [
+        "views/account_payment_order_views.xml",
+    ],
+}
