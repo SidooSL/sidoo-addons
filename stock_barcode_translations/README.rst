752eefb159f3fdc7089aa63973e970e297959a19
Jorge Quinteros <jquinteros@sdi.es>
2025-07-07 09:35:45 +0200

    [ADD] icon image and translations for stock barcode

---
+++ stock_barcode_translations/README.rst
@@ -0,0 +1,25 @@
+# Stock Barcode Translations
+
+Este módulo proporciona traducciones al español para el módulo `stock_barcodes` de Odoo 14.0.
+
+## Características
+- Traduce al español los textos y etiquetas del módulo `stock_barcodes`.
+- Facilita el uso del escaneo de códigos de barras en almacén para usuarios hispanohablantes.
+
+## Instalación
+1. Copie este módulo en su carpeta de addons personalizada.
+2. Asegúrese de tener instalado el módulo `stock_barcodes`.
+3. Instale el módulo desde la interfaz de Odoo o mediante la línea de comandos.
+
+## Dependencias
+- `stock_barcodes`
+
+## Uso
+Una vez instalado, las etiquetas y textos del módulo de códigos de barras de inventario aparecerán traducidos al español.
+
+## Créditos
+- Autor: Jorge Quinteros, SDi
+- Página web: https://www.sdi.es/
+
+## Licencia
+AGPL-3
---
+++ stock_barcode_translations/__manifest__.py
@@ -0,0 +1,31 @@
+###############################################################################
+#
+#    SDi
+#    Copyright (C) 2022-Today SDi <www.sdi.es>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+{
+    "name": "Stock Barcode Translations",
+    "summary": "Traducciones al español para el módulo stock_barcodes",
+    "author": "Jorge Quinteros, SDi,",
+    "website": "https://www.sdi.es/",
+    "license": "AGPL-3",
+    "category": "Customization",
+    "version": "14.0.1.0.0",
+    "depends": [
+        "stock_barcodes",
+    ],
+}
