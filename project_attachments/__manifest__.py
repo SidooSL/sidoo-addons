77428968fe74bc5b43f0b40f9b353dae298a3874
osoto <osoto@sidoo.es>
2025-10-28 15:54:20 +0100

    precommit project_attachments

--- project_attachments/__manifest__.py
+++ project_attachments/__manifest__.py
@@ -1,22 +1,22 @@
 {
-    'name': 'Project Attachments',
-    'version': '18.0.1.0.0',
-    'category': 'Project',
-    'summary': 'Smartbutton to view project and task attachments',
-    'description': """
+    "name": "Project Attachments",
+    "version": "18.0.1.0.0",
+    "category": "Project",
+    "summary": "Smartbutton to view project and task attachments",
+    "description": """
         Adds a smartbutton in project form view to view all attachments
         related to the project and its tasks.
     """,
-    'author': 'Oscar Soto, SDi SL',
-    'website': 'https://www.sdi.es',
-    'depends': ['project', 'base'],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/project_project_views.xml',
+    "author": "Oscar Soto, SDi SL",
+    "website": "https://www.sdi.es",
+    "depends": ["project", "base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/project_project_views.xml",
     ],
-    'demo': [],
-    'installable': True,
-    'auto_install': False,
-    'application': False,
-    'license': 'LGPL-3',
+    "demo": [],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+    "license": "LGPL-3",
 }
