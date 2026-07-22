74ee04c43dbc4417ce7fba8cf47a9a30b1c511eb
Jorge Quinteros <jquinteros@sdi.es>
2026-07-22 14:46:09 +0200

    [ADD] partner_tier_validation_auto

---
+++ partner_tier_validation_auto/__manifest__.py
@@ -0,0 +1,30 @@
+###############################################################################
+#
+#    SDi
+#    Copyright (C) 2026-Today SDi <www.sdi.es>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+{
+    "name": "Partner Tier Validation Automatic",
+    "summary": "Automatically submit and archive contacts pending approval",
+    "author": "Jorge Quinteros, SDi",
+    "website": "https://sdi.es/",
+    "license": "AGPL-3",
+    "category": "Contacts",
+    "version": "18.0.1.0.0",
+    "depends": ["partner_tier_validation"],
+    "data": ["views/res_partner_views.xml"],
+}
